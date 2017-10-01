037235fb20c94b7c799e03176026110c27bf09e8
frankiezeager <frankiezeager@yahoo.com>
2017-09-30 20:16:12 -0500

    fixed dimensions, added model checkpointing

--- trap_generator_RNN.py
+++ trap_generator_RNN.py
@@ -9,33 +9,27 @@ RNN Code to generate trap lyrics based on scraped trap lyric data
 input: trap lyrics scraped from AZ Lyrics using the lyric_scraper.py file
 output: generated lyrics produced by a recurrent neural network
 """
-
 import pickle
-import pandas as pd
-from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding,TimeDistributed,Activation
 from keras.layers import LSTM
-from keras.optimizers import RMSprop
 from keras.callbacks import ModelCheckpoint
 import numpy as np
 from keras.utils import np_utils
-import math
-import string
-import random
 #import lyrics
 lyrics=pickle.load(open('clean_lyrics.p','rb'))
 
-chars = list(set(lyrics))
+chars = sorted(list(set(lyrics)))
 vocab_size=len(chars)
 
 #create mapping from characters to indexes and indexes to characters
-ix_to_char = {ix:char for ix, char in enumerate(chars)}
-char_to_ix = {char:ix for ix, char in enumerate(chars)}
-
+#ix_to_char = {ix:char for ix, char in enumerate(chars)}
+#char_to_ix = {char:ix for ix, char in enumerate(chars)}
+char_to_ix = dict((c, i) for i, c in enumerate(chars))
+ix_to_char = dict((i, c) for i, c in enumerate(chars))
 #create hyperparameters 
 seq_length=100
-hidden_dimension=500
+hidden_dimension=256
 layer_num=3
 batch_size=50000
 generate_length=850
@@ -43,17 +37,19 @@ generate_length=850
 #prep data
 X_input=[]
 y_input=[]
-for i in range(0,math.floor(len(lyrics)/seq_length)): #for i in number of sequences
-    #create sequence of lyrics from full raw text
-    beg=i*seq_length #so this will start at 0 for i=0
-    end=(i+1)*seq_length
-    x_sample=lyrics[beg:end]
-    y_sample=lyrics[end] #this will be the next character after the x sequence (the true predicted value)    
-    X_input.append([char_to_ix[char] for char in x_sample]) #encode for each character in the sequence and append to X
-    y_input.append(char_to_ix[y_sample])
 
-#translate features into the form [sample, t, features]
+for i in range(0,len(lyrics)-seq_length,1):
+    #create sequence of lyrics from full raw text
+    end=i+seq_length #find end of the lryics
+    x_seq=lyrics[i:end]
+    y_seq=lyrics[end]
+    X_input.append([char_to_ix[char] for char in x_seq]) #convert each letter in sequence to ix
+    y_input.append(char_to_ix[y_seq]) #convert letter to ix
+
+#translate features into the form [sample, sequence, features]
 X=np.reshape(X_input,(len(X_input),seq_length,1))
+#X=X_input.reshape(1,seq_length,1)
+#y=Y_input.reshape
 #normalize
 X=X/float(vocab_size)
 #one hot encoding the data
@@ -61,48 +57,63 @@ y=np_utils.to_categorical(y_input)
  
        
 def output_text(model, length=500):
-"""
-Function to output text trained by the neural network. Starts with a randomly selected capital letter.
-Input:
-    model: fit keras model object
-    length: int. how long the output text should be. Default is 500 characters.
-Global variables:
-    vocab_size: int. How long the vocab size is
-"""
-
+    """
+    Function to output text trained by the neural network. Starts with a randomly selected capital letter.
+    Input:
+        model: fit keras model object
+        length: int. how long the output text should be. Default is 500 characters.
+    Global variables:
+        vocab_size: int. How long the vocab size is
+    """
     #get random sample to start generating text
-    ix=np.random.randint(0,len(X)-1)
-    sample=X_input[ix]
+
+    ix=np.random.randint(0,len(X)-1) #get random integer
+    sample=X_input[ix] #get the text from that point
     print("\n")
     print("Random Text Seed from corpus: \n")
     print(''.join(ix_to_char[index] for index in sample)) #print all the letters from the sample
     print()
     seed_text=''.join(ix_to_char[index] for index in sample)
     for i in range(length):
-        x=np.reshape(sample,(1,len(sample),1)) #get in form [samples,time_step,features]
+        x=np.reshape(sample,(1,len(sample),1)) #get in form [samples,sequence,features]
         x=x/float(vocab_size) #normalize
-        pred=model.predict(x,verbose=0) #make prediction for next letter
-        index=np.argmax(pred)
+        pred=model.predict(x)[0] #make prediction for next letter
+        index=np.argmax(pred,axis=1)[-1]
         result=ix_to_char[index]
-        seed_text=seed_text+result #append to text
+        seed_text=seed_text+result#append to text
+        sample.append(index)#add to the sample 
+        sample=sample[1:] #removing first character to keep consistent length
+        
     print("Final Output: \n")
     print(seed_text)
+    return seed_text
 
 #define model
 model=Sequential()
-model.add(LSTM(hidden_dimension,input_shape=(None,vocab_size), return_sequences=True))
+model.add(LSTM(hidden_dimension,input_shape=(X.shape[1],X.shape[2]), return_sequences=True))
 for i in range(layer_num-1):
     model.add(LSTM(hidden_dimension,return_sequences=True))
-model.add(TimeDistributed(Dense(vocab_size)))
-model.add(Activation('softmax'))
-model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+model.add(TimeDistributed(Dense(vocab_size))) #add if you want to try many to many output (instead of character by character)
+#model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss="categorical_crossentropy", optimizer="adam")
+
+
+#see how bad the original model is
+print("initial text output: ")
+output_text(model)
 
 num_epoch=0
 
+text_file=open('text_file.txt','w')
+text_file.close()
 while True:
     print('\n\n')
     model.fit(X, y, batch_size=batch_size, verbose=1, epochs=1)
     num_epoch += 1
     print("Epoch number ", num_epoch)
-    output_text(model, generate_length)
-    ModelCheckpoint('model_chceckpoint_epoch_'+num_epoch+'.hdf5',save_best_only=True)
+    predicted_text=output_text(model, generate_length)
+    text_file=open('text_file.txt','a')
+    text_file.write('epoch number '+ num_epoch+ '\n\n\n' + predicted_text)
+    text_file.close()
+    #pickle.dump(predicted_text,open('text_'+str(num_epoch)+'.p','wb'))
+    ModelCheckpoint('model_chceckpoint_epoch_'+str(num_epoch)+'.hdf5',save_best_only=True)
